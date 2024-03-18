ef8341286a41778760bfb9a0821a8a460b8821fd
VitaliiKyrpach <126057381+VitaliiKyrpach@users.noreply.github.com>
2024-03-18 17:25:20 +0200

    Updates

--- assets/FavoritesPage-L52TYAll.js
+++
@@ -1,1 +0,0 @@
-import{j as r,C as e}from"./index-ds4UqyXr.js";const i=()=>r.jsx("div",{children:r.jsx(e,{children:r.jsx("div",{children:"FavoritesPage"})})});export{i as default};
--- assets/HomePage-Cy8uCczz.js
+++
@@ -1,1 +0,0 @@
-import{j as e,C as r}from"./index-ds4UqyXr.js";const i=()=>e.jsx("div",{children:e.jsx(r,{children:e.jsx("div",{children:"HomePage"})})});export{i as default};
