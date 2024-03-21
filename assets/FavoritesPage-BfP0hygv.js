8b78100eff18a27d82e71e57cc38eb4fae8a5d87
VitaliiKyrpach <vitalij.9f0x9@gmail.com>
2024-03-21 23:36:40 +0200

    Updates

---
+++ assets/FavoritesPage-BfP0hygv.js
@@ -0,0 +1,1 @@
+import{u as a,j as s,C as o}from"./index-DTSEUVC_.js";import{b as e,C as r,a as i}from"./CatalogFilter-BUrUe0YX.js";const c=()=>{const t=a(e);return console.log(t),s.jsx("div",{children:s.jsxs(o,{children:[s.jsx(r,{}),s.jsx(i,{data:t})]})})};export{c as default};
--- assets/FavoritesPage-Dmww10zQ.js
+++
@@ -1,1 +0,0 @@
-import{j as r,C as e}from"./index-DCB8z8xc.js";const i=()=>r.jsx("div",{children:r.jsx(e,{children:r.jsx("div",{children:"FavoritesPage"})})});export{i as default};
